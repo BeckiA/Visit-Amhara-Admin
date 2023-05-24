1e02058c8c5908fb837af66f7b52816421633e39
becki <berekeetabera@gmail.com>
2023-05-24 03:41:06 +0300

    Finishing the display data feature

--- lib/screens/view_attractions.dart
+++ lib/screens/view_attractions.dart
@@ -39,13 +39,145 @@ class ViewAttraction extends StatelessWidget {
               itemCount: attractions.length,
               itemBuilder: (context, index) {
                 final attraction = attractions[index];
-                return ListTile(
-                  leading: Image.network(attraction.picture),
-                  title: Text(attraction.title),
-                  subtitle: Text(attraction.location),
-                  onTap: () {
-                    // Handle attraction item tap
-                  },
+                return Card(
+                  child: Row(children: [
+                    Container(
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              bottomLeft: Radius.circular(10))),
+                      width: 140,
+                      height: 140,
+                      child: Image.network(
+                        attraction.picture,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 12,
+                    ),
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        // crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                attraction.title,
+                                style: const TextStyle(
+                                  fontFamily: "Montserrat",
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          Row(
+                            // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  const Icon(LineAwesomeIcons.map_marker),
+                                  const SizedBox(
+                                    width: 8,
+                                  ),
+                                  Text(
+                                    attraction.location,
+                                    style: const TextStyle(
+                                      fontFamily: "Montserrat",
+                                      fontSize: 16,
+                                    ),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(
+                                width: 8,
+                              ),
+                              Row(
+                                children: [
+                                  const Icon(
+                                      LineAwesomeIcons.identification_badge),
+                                  const SizedBox(
+                                    width: 8,
+                                  ),
+                                  Text(
+                                    attraction.categoryId,
+                                    style: const TextStyle(
+                                      fontFamily: "Montserrat",
+                                      fontSize: 16,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Row(
+                                children: [
+                                  const Text(
+                                    "Latitude:",
+                                    style: TextStyle(
+                                        fontFamily: "Montserrat", fontSize: 14),
+                                  ),
+                                  const SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    attraction.latitude.toString(),
+                                    style: const TextStyle(
+                                        fontFamily: "Montserrat", fontSize: 14),
+                                  )
+                                ],
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Row(
+                                children: [
+                                  const Text(
+                                    "Longitiude:",
+                                    style: TextStyle(
+                                        fontFamily: "Montserrat", fontSize: 14),
+                                  ),
+                                  const SizedBox(
+                                    width: 5,
+                                  ),
+                                  Text(
+                                    attraction.longitiude.toString(),
+                                    style: const TextStyle(
+                                        fontFamily: "Montserrat", fontSize: 14),
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            LineAwesomeIcons.edit,
+                            size: 35,
+                          ),
+                        ),
+                        IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              LineAwesomeIcons.trash,
+                              size: 35,
+                              color: Colors.redAccent,
+                            ))
+                      ],
+                    ),
+                  ]),
                 );
               },
             );
